531e0931288d3a2ac3fb9ab2cb337639a2c408d5
cristianqsanchez <cristianquintero316@gmail.com>
2022-12-11 22:35:20 -0500

    feat (client): add editable properties to tasks :memo:

--- client/src/components/Task/Task.jsx
+++ client/src/components/Task/Task.jsx
@@ -1,8 +1,22 @@
+import { useState } from 'react'
+
 export default function Task({ task }) {
+  const [item, setItem] = useState({ task })
+
+  const handleTitle = e => setItem({ ...item, title: e.target.value })
+  const handleDescription = e => setItem({ ...item, description: e.target.value })
+  const handleDone = e => setItem({ ...item, done: !item.done })
+  const handleSubmit = async () => await fetch(`http://localhost:3000/${task._id}`, {
+    method: "PATCH",
+    body: JSON.stringify(item),
+    headers: { "Content-type": "application/json; charset=UTF-8" }
+  })
   return (
     <article>
-      <h2>{task.title}</h2>
-      <p>{task.description}</p>
+      <input type="text" defaultValue={task.title} onChange={handleTitle} />
+      <input type="text" defaultValue={task.description} onChange={handleDescription} />
+      <button onClick={handleDone}>{'done'}</button>
+      <button onClick={handleSubmit}>{'submit'}</button>
     </article>
   )
 }
