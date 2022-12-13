362a9cc064e4ad84357bc3b11aaa2096c4d3296d
cristianqsanchez <cristianquintero316@gmail.com>
2022-12-12 23:50:34 -0500

    refactor (task): set state properties on render default values :wrench:

--- client/src/components/Task/Task.jsx
+++ client/src/components/Task/Task.jsx
@@ -1,22 +1,22 @@
 import { useState } from 'react'
 
 export default function Task({ task }) {
-  const [item, setItem] = useState({ task })
+  const [taskProps, setTaskProps] = useState({ title: task.title, description: task.description, done: task.done })
 
-  const handleTitle = e => setItem({ ...item, title: e.target.value })
-  const handleDescription = e => setItem({ ...item, description: e.target.value })
-  const handleDone = e => setItem({ ...item, done: !item.done })
+  const handleTitle = e => setTaskProps({ ...taskProps, title: e.target.value })
+  const handleDescription = e => setTaskProps({ ...taskProps, description: e.target.value })
+  const handleDone = e => setTaskProps({ ...taskProps, done: !taskProps.done })
   const handleSubmit = async () => await fetch(`http://localhost:3000/${task._id}`, {
     method: "PATCH",
-    body: JSON.stringify(item),
+    body: JSON.stringify(taskProps),
     headers: { "Content-type": "application/json; charset=UTF-8" }
   })
   return (
     <article>
-      <input type="text" defaultValue={task.title} onChange={handleTitle} />
-      <input type="text" defaultValue={task.description} onChange={handleDescription} />
-      <button onClick={handleDone}>{'done'}</button>
-      <button onClick={handleSubmit}>{'submit'}</button>
+      <input type="text" defaultValue={taskProps.title} onChange={handleTitle} />
+      <input type="text" defaultValue={taskProps.description} onChange={handleDescription} />
+      <button onClick={handleDone}>{JSON.stringify(taskProps.done)}</button>
+      <button onClick={handleSubmit}>submit</button>
     </article>
   )
 }
