999c12e41c2d338202d0b920140a0c5d03be1203
cristianqsanchez <cristianquintero316@gmail.com>
2022-12-21 19:53:18 -0500

    test (tasks): add tasks endpoint tests

--- server/src/tests/tasks.test.js
+++ server/src/tests/tasks.test.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import taskModel from '../tasks/model/tasks.model.js'
-import { api, initialTasks } from './helpers/test.helpers'
+import { api, initialTasks, getTasks } from './helpers/test.helpers'
 
 dotenv.config()
 
@@ -18,8 +18,53 @@ afterEach(async () => {
   await mongoose.connection.close()
 })
 
-test('tasks returned as json', async () => {
-  await api.get('/')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+describe('HTTP:GET /', () => {
+  test('should return as json', async () => {
+    await api
+      .get('/')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+  test('should return all tasks', async () => {
+    const taskList = await getTasks()
+    expect(taskList).toHaveLength(initialTasks.length)
+  })
+  test('should return a specific task', async () => {
+    const taskList = await getTasks()
+    const { title, description } = taskList[0]
+    expect({ title, description }).toStrictEqual(initialTasks[0])
+  })
+})
+
+describe('HTTP:POST /', () => {
+  test('should create a task to do', async () => {
+    const newTask = { title: 'new task', description: 'new description' }
+    await api
+      .post('/')
+      .send(newTask)
+      .expect(200)
+    const taskList = await getTasks()
+    expect(taskList).toHaveLength(initialTasks.length + 1)
+  })
+})
+
+describe('HTTP:PATCH /', () => {
+  test('should edit a task', async () => {
+    const taskList = await getTasks()
+    const task = taskList[0]
+    await api
+      .patch(`/${task._id}`)
+      .send({ description: 'new description' })
+      .expect(200)
+  })
+})
+
+describe('HTTP:DELETE /', () => {
+  test('should delete a task', async () => {
+    const taskList = await getTasks()
+    const task = taskList[0]
+    await api
+      .delete(`/${task._id}`)
+      .expect(200)
+  })
 })
