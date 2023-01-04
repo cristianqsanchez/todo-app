6c0315baf49acf9e4ddf14e5c6e52b32331d3b50
cristianqsanchez <cristianquintero316@gmail.com>
2023-01-03 22:48:04 -0500

    feat (server): add users router in express app :sparkles:

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import tasksRouter from './tasks/routes/tasks.routes.js'
+import usersRouter from './users/routes/users.routes.js'
 
 const app = express()
 
 app.use(express.json())
 
 app.use(tasksRouter)
+app.use('/users', usersRouter)
 
 export default app
